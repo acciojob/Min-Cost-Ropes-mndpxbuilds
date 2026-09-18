7fd4d16de23194c458679276f7897d592626aa4c
Mandeep Sharma <mandeepvashist921@gmail.com>
2026-09-18 12:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let sum = 0;
+	arr.sort();
+	let arr2;
+	for(let i=0; i<arr.length; i++){
+		let r1 = arr[i]+arr[i+1];
+		r1+=i;
+		arr2.push_back(r1);
+	}
+  for(let j=0; j<arr2.length; j++){
+	  sum+=arr2[j];
+  }
+	return sum;
 }
 
 module.exports=mincost;
