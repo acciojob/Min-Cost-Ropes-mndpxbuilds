63b1dba87be3e9cfa6a3701564d1d8adf1d0279c
Mandeep Sharma <mandeepvashist921@gmail.com>
2026-09-18 14:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ function mincost(arr)
 //write your code here
 // return the min cost
 	let sample = 0;
-	let sum = 0;
+	//let sum = 0;
 	let sum2 = 0;
 	arr.sort();
 	let arr2 = [];
 	for(let i=0; i<1; i++){
 			sample=arr[i]+arr[i+1];
-		arr2.push(sample);
+		  arr2.push(sample);
 	}
   for(let j=2; j<arr.length; j++){
-	   sum = sample+arr[j];
-	  arr2.push(sum);
+	   sample = sample+arr[j];
+	  
+	  arr2.push(sample);
   }
 	for(let k=0; k<arr2.length; k++){
 		sum2+=arr2[k];
